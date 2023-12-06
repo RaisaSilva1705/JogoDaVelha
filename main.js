a8212d132e63fe54fd28acb6516e4d9dbfd50fb6
Raisa Silva <140858752+RaisaSilva1705@users.noreply.github.com>
2023-12-06 17:17:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -49,20 +49,55 @@ function ModoDeJogo(modoJogo){
     }
 }
 
+function jogoRobo(){
+    if (jogador2 == false){
+        var tabuleiro = document.querySelectorAll(".cell");
+        tabuleiro = tabuleiro[Math.floor(Math.random() * (8 - 0)) + 0];
+        
+        if (cell.innerText === '' && !checkWinner())
+            cellClick(tabuleiro);
+    }
+}
+
 function mostrarJogadorAtual(){
     document.getElementById('jogadorAtual').innerHTML = 'Jogador atual: ' + currentPlayer;
 }
 
 let currentPlayer = 'X';
+let tabuleiro = null;
+let validarPosi = null;
 
 function cellClick(cell, pos) {
     if (jogadorCadastrado == true){
-        if (cell.innerText === '' && posiCell[pos] == undefined){
+        if (cell.innerText === '' && !checkWinner()){
             cell.innerText = currentPlayer;
+            posiCell[pos] = currentPlayer;
+
+            checkWinner();  
+            checkEmpate();
+
+            // robo escolhe uma posição aleatória que não esteja ocupada
+            if (jogador2 == false && currentPlayer == 'X'){
+                tabuleiro = document.querySelectorAll(".cell");
+
+                validarPosi = tabuleiro[Math.floor(Math.random() * (8 - 0)) + 0];
+                
+                while(validarPosi.innerText !== ''){
+                    validarPosi = tabuleiro[Math.floor(Math.random() * (8 - 0)) + 0];
+                }
+            }
+
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; // ? -> operador ternário
+            
+            // robo joga na posição aleatória que tinha escolhido anteriormente
+            if (permitirRobo == true && currentPlayer == 'O'){
+                validarPosi.innerText = 'O';
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                posiCell[pos] = currentPlayer;
+                console.log("2 if");
+            }
 
             mostrarJogadorAtual();
-            checkWinner();
         }
     }
     else{
@@ -74,23 +109,29 @@ function resetPagina(){
     window.location.reload();
 }
 
+function resetJogo(){
+    setTimeout(() => {
+        var limpar = document.querySelectorAll('.cell');
+        for (var i = 0; i <= limpar.length; i++)
+            limpar[i].innerText = '';
+    }, [2000]);
+    delete posiCell;
+    posiCell = new Array(9);
+    permitirRobo = true;
+    
+}
+
 /* Fazer a verificação do vencedor.
    Pode ocorrer vitória do jogador 1 ou 2, bem como empate.
    Incrementar contador do vencedor
  */
 
 let posiCell = new Array(9);
-/* 
-    0 1 2
-    3 4 5
-    6 7 8
-*/
-let vitLinha = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-let vitColuna = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
-let vitDiagonal = [[0, 4, 8], [2, 4, 6]];
 
 let contVitoria1 = 0;
 let contVitoria2 = 0;
+
+let permitirRobo = true;
 
 function checkEmpate(){
     var cellPreenchidos = 0;
@@ -99,54 +140,79 @@ function checkEmpate(){
         if(posiCell[i] != undefined)
             cellPreenchidos++;
     }
+    if(cellPreenchidos == 9){
+        alert("Empate!");
+        resetJogo();
+        cellPreenchidos = 0;
+    }
     return cellPreenchidos == posiCell.length;
-}
-
-function checkLinha(){
-    if(posiCell[vitLinha] == 'X'){
-        contVitoria1++;
-        document.getElementById('player1-wins').innerHTML = 'Vitórias: ' + contVitoria1; 
-    }
-    if(posiCell[vitLinha] == '0'){
-        contVitoria2++;
-        document.getElementById('player2-wins').innerHTML = 'Vitórias: ' + contVitoria2;
-    }
-}
-
-function checkColuna(){
-    if(posiCell[vitColuna] == 'X'){
-        contVitoria1++;
-        document.getElementById('player1-wins').innerHTML = 'Vitórias: ' + contVitoria1;
-        return true;
-    }
-    else if(posiCell[vitColuna] == '0'){
-        contVitoria2++;
-        document.getElementById('player2-wins').innerHTML = 'Vitórias: ' + contVitoria2;
-        return true;
-    }
-    else if(null){
-        return false;
-    }
-}
-
-function checkDiagonal(){
-    if(posiCell[vitDiagonal] == 'X'){
-        contVitoria1++;
-        document.getElementById('player1-wins').innerHTML = 'Vitórias: ' + contVitoria1;
-    }
-    if(posiCell[vitDiagonal] == '0'){
-        contVitoria2++;
-        document.getElementById('player2-wins').innerHTML = 'Vitórias: ' + contVitoria2;
-    }
 }
 
 function checkWinner(){
     checkLinha();
     checkColuna();
     checkDiagonal();
-    checkEmpate();
+}
 
-    if (checkLinha() == true || checkColuna() == true || checkDiagonal() == true || checkEmpate() == true){
-        resetPagina();
-    }    
+function checkLinha(){
+    for (var i = 0; i < 7; i += 3){
+        if(posiCell[i] == 'X' && posiCell[i + 1] == 'X' && posiCell[i + 2] == 'X'){
+            contVitoria1++;
+            document.getElementById('player1-wins').innerHTML = 'Vitórias: ' + contVitoria1;
+
+            document.getElementById('jogadorAtual').innerHTML = "Vitória do Jogador 1!";
+            permitirRobo = false;
+            resetJogo();
+        }
+        if(posiCell[i] == 'O' && posiCell[i + 1] == 'O' && posiCell[i + 2] == 'O'){
+            contVitoria2++;
+            document.getElementById('player2-wins').innerHTML = 'Vitórias: ' + contVitoria2;
+
+            document.getElementById('jogadorAtual').innerHTML = "Vitória do Jogador 2!";
+            permitirRobo = false;
+            resetJogo();
+        }
+    }
+}
+
+function checkColuna(){
+    for (var i = 0; i < 3; ++i){
+        if(posiCell[i] == 'X' && posiCell[i + 3] == 'X' && posiCell[i + 6] == 'X'){
+            contVitoria1++;
+            document.getElementById('player1-wins').innerHTML = 'Vitórias: ' + contVitoria1;
+
+            document.getElementById('jogadorAtual').innerHTML = "Vitória do Jogador 1!";
+            permitirRobo = false;
+            resetJogo();
+        }
+        if(posiCell[i] == 'O' && posiCell[i + 3] == 'O' && posiCell[i + 6] == 'O'){
+            contVitoria2++;
+            document.getElementById('player2-wins').innerHTML = 'Vitórias: ' + contVitoria2;
+
+            document.getElementById('jogadorAtual').innerHTML = "Vitória do Jogador 2!";
+            permitirRobo = false;
+            resetJogo();
+        }
+    }
+}
+
+function checkDiagonal(){
+    if((posiCell[0] == 'X' && posiCell[4] == 'X' && posiCell[8] == 'X') || (
+        posiCell[2] == 'X' && posiCell[4] == 'X' && posiCell[6] == 'X')){
+        contVitoria1++;
+        document.getElementById('player1-wins').innerHTML = 'Vitórias: ' + contVitoria1;
+
+        document.getElementById('jogadorAtual').innerHTML = "Vitória do Jogador 1!";
+        permitirRobo = false;
+        resetJogo();
+    }
+    if((posiCell[0] == 'O' && posiCell[4] == 'O' && posiCell[8] == 'O') || (
+        posiCell[2] == 'O' && posiCell[4] == 'O' && posiCell[6] == 'O')){
+        contVitoria2++;
+        document.getElementById('player2-wins').innerHTML = 'Vitórias: ' + contVitoria2;
+
+        document.getElementById('jogadorAtual').innerHTML = "Vitória do Jogador 2!";
+        permitirRobo = false;
+        resetJogo();
+    }
 }
